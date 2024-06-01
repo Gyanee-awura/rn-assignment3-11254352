572b221a62dfa9982af3a999ae63939382bee47f
richmond-andoh <richmondandoh00@gmail.com>
2024-06-01 15:32:47 +0000

    Some fixes

--- sreen/HomeScreen.jsx
+++ sreen/HomeScreen.jsx
@@ -1,5 +1,4 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
 import Header from '../components/Header'
 import Category from '../components/Category'
 import Task from '../components/Task'
